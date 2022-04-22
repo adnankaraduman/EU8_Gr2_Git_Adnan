7c46f92708839d7588a8b2bc765fb127287bbb33
ado <akrdmn66@gmail.com>
2022-04-22 21:33:31 +0300

    AC1 is completed

--- src/Login_Test_Adnan.java
+++ src/Login_Test_Adnan.java
@@ -4,6 +4,9 @@ public class Login_Test_Adnan {
 
         System.out.println("Initial change and first class");
 
+        System.out.println("AC1 testing valid credentials is passed");
+
+
 
     }
 
